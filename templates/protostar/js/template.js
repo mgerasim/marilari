a0d9a2f5666e51f7d327e99b900ec4e5fa2403e1
Mikhail Gerasimov <mgerasim@inbox.ru>
2014-10-13 12:45:19 +1100

    aside

--- templates/protostar/js/template.js
+++ templates/protostar/js/template.js
@@ -41,5 +41,27 @@
 				$("label[for=" + $(this).attr('id') + "]").addClass('active btn-success');
 			}
 		});
+		
+		$(function() {
+
+			var $sidebar   = $("#aside"), 
+				$window    = $(window),
+				offset     = $sidebar.offset(),
+				topPadding = 15;
+
+			$window.scroll(function() {
+				if ($window.scrollTop() > offset.top) {
+					$sidebar.stop().animate({
+						marginTop: $window.scrollTop() - offset.top + topPadding
+					});
+				} else {
+					$sidebar.stop().animate({
+						marginTop: 0
+					});
+				}
+			});
+			
+		});
+
 	})
 })(jQuery);
